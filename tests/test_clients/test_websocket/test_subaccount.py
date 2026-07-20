de1a29d360af061b1888c6630fb96e3adcb24684
Zarathustra <zarathustra_git@pm.me>
2026-07-20 20:34:29 +0200

    test: test_subaccount_setup_config

---
+++ tests/test_clients/test_websocket/test_subaccount.py
@@ -0,0 +1,12 @@
+"""Tests for Subaccount module."""
+
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_subaccount_setup_config(client_owner_wallet):
+    assert client_owner_wallet.active_subaccount.margin_type == "SM"
+    assert client_owner_wallet.active_subaccount.state.manager_id == 1
+    assert client_owner_wallet.active_subaccount.state.risk_universe_id == 1
+    currency = client_owner_wallet.active_subaccount.currency
+    assert isinstance(currency, list)
