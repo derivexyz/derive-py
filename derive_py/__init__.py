7f8c9dbd8ab38039b448e512815b856555d28cd4
Zarathustra <zarathustra_git@pm.me>
2026-08-15 11:52:52 +0000

    feat: re-export wait_for_settlement from derive_py

--- derive_py/__init__.py
+++ derive_py/__init__.py
@@ -1,9 +1,10 @@
 """Derive client package."""
 
-from ._clients import AsyncHTTPClient, HTTPClient, WebSocketClient
+from ._clients import AsyncHTTPClient, HTTPClient, WebSocketClient, wait_for_settlement
 
 __all__ = [
     "HTTPClient",
     "AsyncHTTPClient",
     "WebSocketClient",
+    "wait_for_settlement",
 ]
--- derive_py/_clients/__init__.py
+++ derive_py/_clients/__init__.py
@@ -2,10 +2,12 @@
 
 from .rest.async_http.client import AsyncHTTPClient
 from .rest.http.client import HTTPClient
+from .utils import wait_for_settlement
 from .websockets.client import WebSocketClient
 
 __all__ = [
     "HTTPClient",
     "AsyncHTTPClient",
     "WebSocketClient",
+    "wait_for_settlement",
 ]
